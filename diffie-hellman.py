b6ee966509598f0bc701bf9a1cc986465cd97b77
Rafael Nunes <rafael.nunes.dev@gmail.com>
2016-09-16 13:25:31 -0300

    Adicionada lógica para encriptar e decriptar mensagens

--- diffie-hellman.py
+++ diffie-hellman.py
@@ -1,12 +1,52 @@
 import random
+from simplecrypt import encrypt, decrypt
+
+#Funcao para encontrar numero primo aleatorio dentro de um range
+def randomPrimo(ini, fim):
+	nrPrimo = random.randrange(ini, fim)
+	ehPrimo = 0
+	contPrimo = 0
+
+	while ehPrimo == 0:
+		i = 1
+		while i < 6:
+			i += 1
+			if ((i**(nrPrimo-1)) % nrPrimo) == 1:
+				contPrimo += 1
+			else:
+				i = 6
+
+		if contPrimo == 5:
+			ehPrimo = 1
+		else:
+			ehPrimo = 0
+			contPrimo = 0
+			nrPrimo = random.randrange(ini, fim)
+
+	#print "%d ? %d" % (nrPrimo, ehPrimo)
+
+	return nrPrimo
 
 #No caso do exercicio foi solicitado que os valores fossem P = 23 e g = 8, pois devem ser primos entre si.
-p = 23
-g = 8
+#p = 23
+#g = 8
+
+p = randomPrimo(2, 2048)
+g = randomPrimo(1, p-1)
+
+bitsChave = 0
+
+while bitsChave != 8 and bitsChave != 16 and bitsChave != 32 and bitsChave != 64 and bitsChave != 128:
+	bitsChave = input("Informe o tamanho da chave que deseja utilizar (8, 16, 32, 64, 128): ")
+
+chaveMax = (2**bitsChave)-1
+
+a = random.randrange(1, chaveMax)
+b = random.randrange(1, chaveMax)
 
 #Necessario gerar os valores aleatorios para "a" e "b", utilizando o limite de 5000 pois no Diffie Helmann geralmente utilizasse numeros grandes.
-a = random.randrange(1, 5000)
-b = random.randrange(1, 5000)
+#a = random.randrange(1, 5000)
+#b = random.randrange(1, 5000)
 
 #Geracao das chaves consideradas "Publicas" que seriam trocadas entre as maquinas
 A = (g**a)%p
@@ -16,11 +56,22 @@ B = (g**b)%p
 ka = (B**a)%p
 kb = (A**b)%p
 
-print "Chave A = %d" % ka
-print "Chave B = %d" % kb
+#print "Chave A = %d" % ka
+#print "Chave B = %d" % kb
 
 #Caso as chaves sejam iguais a operacao ocorreu corretamente, sendo possivel que um decifre a mensagem do outro
 if ka == kb:
 	print "Chaves geradas corretamente"
 else:
 	print "Chaves geradas incorretamente"
+
+msg = raw_input("Digite a mensagem para enviar para 'B': ")
+
+#encripta a mensagem
+msgEnc = encrypt(str(ka), msg)
+
+#decripta a partir da mensagem encriptada
+msgDec = decrypt(str(kb), msgEnc)
+
+print "Mensagem de A decifrada em B:"
+print msgDec
